4182142180d59aa1edc74230435f88809d03bbf7
Tonatiuh Ramos <123381053+Tonaram@users.noreply.github.com>
2023-04-24 00:57:50 -0600

    Update tareas.component.ts

--- gestor-de-tareas/src/app/tareas/tareas.component.ts
+++ gestor-de-tareas/src/app/tareas/tareas.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { TareasService } from '../servicios/tareas.service';
+// src\app\servicios\tareas.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Component({
-  selector: 'app-tareas',
-  templateUrl: './tareas.component.html',
-  styleUrls: ['./tareas.component.scss']
+@Injectable({
+  providedIn: 'root'
 })
+export class TareasService {
+  private apiUrl = 'http://localhost:3000/api/tasks';
+  
+  constructor(private http: HttpClient) { }
 
-export class TareasComponent implements OnInit {
-  tasks: any[] = [];
-
-  constructor(private tareasService: TareasService) { }
-
-  ngOnInit(): void {
-    this.loadTasks();
+  createTask(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
   }
 
-  loadTasks(): void {
-    this.tareasService.getTasks().subscribe(
-      data => {
-        this.tasks = data;
-      },
-      error => {
-        console.error('Error fetching tasks:', error);
-      }
-    );
+  getTasks(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getTask(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  updateTask(id: string, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteTask(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
